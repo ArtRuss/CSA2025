62af61a2f0457f190e405aaebd2ad2ba4d685497
Allie Britton <alliebritton@macbookair.dhcp.nd.edu>
2025-06-04 15:05:15 -0400

    play sound for 5 seconds

--- challenge1.py
+++ challenge1.py
@@ -1,3 +1,8 @@
+
+from francislib import temperature, sounds, time
+
+
+
 def cooking_alarm(measure_temperature):
     
     cooking_alarm = False
@@ -8,7 +13,18 @@ def cooking_alarm(measure_temperature):
     return cooking_alarm
 
 while True:
-    water_temperature = float(input("What's the water temperature? "))
+
+    # get the temperature form francislib 
+    water_temperature = temperature.get_temperature()
+
+    # call the built function to say if the alarm should sound (True) or not (False)
     alarm_state = cooking_alarm(water_temperature)
 
+    # if True, it will sound, if False, it wont
+    sounds.set_alarm(alarm_state)
+
+    # print test
     print(alarm_state)
+
+    # use time funciton to wait a few seconds to test again
+    time.sleep(5)
