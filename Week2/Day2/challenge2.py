f5ca0ca264284b7359a010469d2fc8ecceb4979a
Matias-G-Toro <mtoro@nd.edu>
2025-06-04 15:38:51 -0400

    Revert "Matias toro"

--- Week2/Day2/challenge2.py
+++
@@ -1,9 +0,0 @@
-# 1.0
-def compute_coordinates():
-    return [(6i - 24,0) for i in range(6)]
-
-# 2.0
-def compute_coordinates(widths: list[int]):
-    pass
-
-#   • I was too ambiguous on what this question was asking
