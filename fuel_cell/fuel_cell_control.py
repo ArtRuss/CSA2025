294acfd0a9f81f29a05a76a6363f0435fff4978a
Kevin Fox <kevinfox@mac.dhcp.nd.edu>
2025-06-04 14:54:34 -0400

    Version 1.1 of fuel cell example

---
+++ fuel_cell/fuel_cell_control.py
@@ -0,0 +1,18 @@
+from arturolib import temperature
+def manageTemp(measured_temp)
+    if(heat_on):
+        if(measured_temp >= 25):
+            heat_on = False
+    elif(cool_on):
+        if(measured_temp <= 25):
+            cool_on = False
+    else: 
+        if(measured_temp <= 22):
+            heat_on = True
+        elif(measured_temp >= 28):
+            cool_on = True
+if __name__ == "__main__":
+    while True:
+        measured_temp = temperature.get_temperature()
+        manageTemp(measured_temp)
+        time.sleep(1)  # Check every 1 second
