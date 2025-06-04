ff19ba11be23b889fb93999798b2d92333bf99ff
Allie Britton <alliebritton@macbookair.dhcp.nd.edu>
2025-06-04 15:40:11 -0400

    add comments

--- challenge2.py
+++ challenge2.py
@@ -3,9 +3,11 @@ def pem_system(get_temperature)
     heat_on = False
     cool_on = False
 
+    # if temp bellow 22, turn on heat
     if get_temperature < 22:
         heat_on = True
-    
+
+    # if temp over 28, trun off heat
     elif get_temperature > 28:
         cool_on = True
 
