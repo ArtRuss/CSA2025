fc0e0c27c9bf2edca8a6930414f28edbe60492b4
Matthew Zampino <mzampino@nd.edu>
2025-06-11 14:26:26 -0400

    sort into week folders

---
+++ Week2/6-4-1.py
@@ -0,0 +1,13 @@
+# Healthy Cooking v1.1.1
+
+from francislib import temperature
+from francislib import sounds
+import time
+
+while True:
+    if temperature.get_temperature() >= 100:
+        for i in range(5, 0, -1):
+            sounds.set_alarm(True)
+            time.sleep(i)
+            sounds.set_alarm(False)
+            time.sleep(1)
