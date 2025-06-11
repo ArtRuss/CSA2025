3ffde29ac41b81856a639c29701588b03f9bac10
Allie Britton <alliebritton@macbookair.dhcp.nd.edu>
2025-06-11 19:58:11 -0400

    data set

--- decision_tree/data_set.py
+++ decision_tree/data_set.py
@@ -9,8 +9,10 @@ x_points = []
 for i in range(30):
     legs = random.choice([2, 4])
     tail = random.choice([0, 1])
-    x_points.append((legs, tail))
+    class_ = random.choice(["dog", "flamingo", "frog"])
+    x_points.append((legs, tail, class_))
 
 # print each point
 for i in x_points:
     print(i)
+
