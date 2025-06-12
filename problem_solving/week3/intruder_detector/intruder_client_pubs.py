fc7dfc4a5bb4a97665f5eb56fd87d534e8293fd4
Kevin Fox <kfox24@nd.edu>
2025-06-12 11:02:26 -0400

    Add SecuritySystem class

--- problem_solving/week3/intruder_detector/intruder_client_pubs.py
+++ problem_solving/week3/intruder_detector/intruder_client_pubs.py
@@ -7,46 +7,53 @@ from paho.mqtt.client import CallbackAPIVersion
 from francislib import irsensor
 from francislib import sounds
 
-def ask_user():
-    """""
-    global intruder_present
-    if input ("Is there an intruder present? (yes/no): ").strip().lower() == "yes":
-        intruder_present = True  
-    else:
-        intruder_present = False
-    """""
-    global intruder_present
-    global alarm_value
-    intruder_present = irsensor.get_state()
-    if intruder_present:
-        sounds.set_alarm(True)
-        alarm_value = "On"
-    else:
-        sounds.set_alarm(False)
-        alarm_value = "Off"
-
-        
+class SecuritySystem:
+    """ A class to represent the security system for an intruder detector. """
+    def __init__(self, user_name):
+        self.user_name = user_name
+        self.intruder_present = False
+        self.alarm_on = False
+        # To make output more readable, we will use messages instead of boolean values
+        self.intruder_message = "No intruder detected"
+        self.alarm_message = "Alarm is OFF"
     
+    def get_user_name(self):
+        return self.user_name
+    
+    def get_sensor(self):
+        return irsensor.get_state()
 
+    """ Checks the state of the sensor and updates the alarm accordingly. """
+    def check_security(self):
+        intruder_present = self.get_sensor()
+        if intruder_present:
+            sounds.set_alarm(True) #Physically turns on the rasperry pi buzzer
+            self.alarm_on = True
+            self.alarm_message = "ON"
+            self.intruder_message = "Intruder detected!"
+        else:
+            sounds.set_alarm(False) #Physically turns off the rasperry pi buzzer
+            self.alarm_on = False
+            self.alarm_message = "OFF"
+            self.intruder_message = "No intruder detected"
+            
 if __name__ == "__main__":
-    user_name = "Kevin_Fox"
-    intruder_present = False  # Default value, can be changed by user input
+    user_name = input("Please enter your user name: ").strip()
+    s1 = SecuritySystem(user_name)
+    
     local_broker = "broker.hivemq.com"
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "intruder_detector")
+    client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2,
+                         client_id = "intruder_detector")
     client.connect(local_broker, 1883, keepalive = 15)
     client.loop_start()
 
     try:
         while True:
-            ask_user()
-            
-            #Read value from sensor
-            #intruder_present = True
-        
-            
-            payload = {"User": user_name, "intruder": intruder_present, "alarm": alarm_value}
-            
-            client.publish("Kevin_Fox/intruder/alarm_value", json.dumps(payload), qos=0)
+            s1.check_security() #checks the state of the sensor and updates the alarm accordingly
+            payload = {"User": user_name, "intruder": s1.intruder_message, "alarm": s1.alarm_message}
+            topic = f"{s1.get_user_name()}/intruder/alarm_value"
+            client.publish(topic, json.dumps(payload), qos=0)
+
     except KeyboardInterrupt:
         print("Exiting the program")
 
--- problem_solving/week3/intruder_detector/intruder_client_subs.py
+++ problem_solving/week3/intruder_detector/intruder_client_subs.py
@@ -6,32 +6,35 @@ import paho.mqtt.client as mqtt
 from paho.mqtt.client import CallbackAPIVersion
 from francislib import irsensor
 
-message_rec = {}
+#message_rec = {}
 
 def on_message(client, userdata, message):
-    global message_rec
+    #global message_rec
     try:
         data = json.loads(message.payload.decode('utf-8'))
         print(message.topic)
         print(data)
+        return data
     except json.JSONDecodeError:
-        data = {}
         print("Received invalid JSON payload")
-    message_rec = data
+        return None
+    # Uncomment the line below if you want to store the received message in a global variable
+    #message_rec = data
+    
 
 
 if __name__ == "__main__":
     local_broker = "broker.hivemq.com"
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "intruder_detector_subscriber")
+    client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2, client_id="intruder_detector_subscriber")
     client.on_message = on_message
     client.connect(local_broker, 1883, keepalive=15)
-    client.subscribe("Kevin_Fox/intruder/alarm_value", qos=0)
+    client.subscribe("Kevin_Fox/security", qos=0)
     client.loop_start()
     try:
         while True:
             print("Waiting for message")
             time.sleep(1)
-            if(message_rec):
+            if(on_message):
                 break
     except KeyboardInterrupt:
         print("Program interrupted by user")
