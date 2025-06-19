b415cd49b950ddab7d4f4abc1472cdd03136ad16
Allie Britton <alliebritton@macbookair.dhcp.nd.edu>
2025-06-18 21:57:58 -0400

    Adding all files to one folder

--- pem_system/arturolib.py
+++ pem_system/arturolib.py
@@ -2,20 +2,29 @@ import random
 
 def get_temperature():
     '''simulates getting the temperature'''
-    temperature = random.randint(80,150)
+    temperature = random.randint(20,35)
     print("The temperature is: ", temperature)
     return temperature
 
-def set_heat(temperature, heat_state):
+def cell_stack():
+    '''simulates the cell stack of temperatures'''
+    cell_stack = [] # initialize empty cell stack
+    for i in range(10):
+        cell_stack.append(get_temperature)
+    return cell_stack
+
+
+def set_heat(cell_number, heat_state):
     '''turns heat on/off'''
-    print("Temperature: ", temperature)
+    print("Temperature: ", cell_number)
     if heat_state:
         print("HEAT ON")
     else:
         print("HEAT OFF")
 
-def set_cold(temperature, cold_state):
+def set_cool(cell_number, cold_state):
     '''turns cold on/off'''
+    print("Temperature: ", cell_number)
     if cold_state:
         print("COLD ON")
     else:
--- pem_system/pem_system2.2.py
+++ pem_system/pem_system2.2.py
@@ -1,25 +1,27 @@
-from arturolib import temperature
+from arturolib import get_temperature, set_heat, set_cool, cell_stack
 
-def pem_system(get_temperature)
+def pem_system(temperature):
     
     heat_on = False
     cool_on = False
 
     # if temp bellow 22, turn on heat
-    if get_temperature < 22:
-        while get_temperature < 25: # keep heat on while its below 25
+    if temperature < 22:
+        while temperature < 25: # keep heat on while its below 25
             heat_on = True
     
     # if temp over 28, trun off heat
-    elif get_temperature > 28:
-        while get_temperature > 25: # keep cold on while its above 28
+    elif temperature > 28:
+        while temperature > 25: # keep cold on while its above 28
             cool_on = True
 
     return heat_on, cool_on
 
-for i in range(cell_stack)
-    temperature = temperature.get_temperature(i)
-    heat_state, cool_state = pem_system(temperature)
+stack_temperature = cell_stack()
+
+for i in range(len(stack_temperature)):
+    temperature = stack_temperature[i]
+    heat_state, cool_state = pem_system(get_temperature)
     set_heat(i, heat_state)
     set_cool(i, cool_state)
     
